d406aec417ba570a89a5eb0b6a9119c387ac1ba5
Moslem Ajra <ajra.engineering@gmail.Com>
2023-10-11 09:54:10 +0100

    Add Cards loading skeletons

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -10,12 +10,8 @@ interface Props {
 
 const GameCard = ({ game }: Props) => {
   return (
-    <Card>
-      <Image
-        borderRadius={10}
-        src={getCroppedImageUrl(game.background_image)}
-        overflow="hidden"
-      />
+    <Card width="300px" borderRadius={10} overflow="hidden">
+      <Image src={getCroppedImageUrl(game.background_image)} />
       <CardBody>
         <Heading fontSize="2xl">{game.name}</Heading>
         <HStack justifyContent={'space-between'}>
--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -12,8 +12,8 @@ export interface Game {
   id: number;
   name: string;
   background_image: string;
-  parent_platforms: {platform: Platform}[],
-  metacritic: number
+  parent_platforms: { platform: Platform }[];
+  metacritic: number;
 }
 
 interface FetchGamesResponse {
@@ -24,26 +24,31 @@ interface FetchGamesResponse {
 const useGames = () => {
   const [games, setGames] = useState<Game[]>([]);
   const [error, setError] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+
 
   useEffect(() => {
     const controller = new AbortController();
+    setIsLoading(true);
     apiClient
       .get<FetchGamesResponse>('/games', { signal: controller.signal })
       .then((res) => {
         setGames(res.data.results);
+        setIsLoading(false);
       })
       .catch((error) => {
         if (error instanceof CanceledError) {
           return;
         }
         setError(error.message);
+        setIsLoading(false);
       });
 
     return () => {
       controller.abort();
     };
   }, []);
-  return { games, error };
+  return { games, error, isLoading };
 };
 
 export default useGames;
